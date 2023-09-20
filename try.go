24e333de937612868ffe19a975a2ea0b775afcdf
Mike5xe <joysmc2@gmail.com>
2023-09-21 01:15:34 +0300

    types of data learning 1

--- try.go
+++ try.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("SHalom, govoru je")
+	var hello2 = "ShalomVsem"       //Один из вариантов обозначения переменной
+	var hello3 = "Osobenniy Shalom" //Значение "string" определилось само.
+	hello := "Shalom"               //Двоеточие перед равно выводит тип данных из присваеваемого значения
+	var (
+		name string = "somebody"
+		age  int
+	) //Вариант в виде списка
+	fmt.Println(hello2, hello, name, age, hello3)
 }
+
+// Если возраст больше 35 - особенный шалом, спростиь возраст.
